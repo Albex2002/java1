f2f46f78c0cbf159d4925d4e50db09e015c5da8a
sebitommy123 <sebi.szafir@hotmail.com>
2016-04-23 17:07:38 +0200

    cazafantasmas en proceso 2
    nose

--- Plugin/src/plugin/PluginClass.java
+++ Plugin/src/plugin/PluginClass.java
@@ -1,6 +1,7 @@
 package plugin;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.bukkit.Color;
 import org.bukkit.GameMode;
@@ -13,8 +14,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -27,12 +30,17 @@ import org.bukkit.scheduler.BukkitRunnable;
 import net.md_5.bungee.api.ChatColor;
 
 public class PluginClass extends JavaPlugin implements Listener {
-	long tiempoUltimoGolpe = 0;
+	
 	long tiempoCooldown = 0;
 
+	
+	private HashMap<Player, Long> tiempos = new HashMap<>();
+	
 	private ArrayList<Player> jugadores = new ArrayList<Player>();
 	private ArrayList<Player> cazafantasmas = new ArrayList<Player>();
 	private ArrayList<Player> fantasmas = new ArrayList<Player>();
+	private ArrayList<Player> listaNoCiegos = new ArrayList<Player>();
+	private ArrayList<Player> listaCooldown = new ArrayList<Player>();
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -42,12 +50,13 @@ public class PluginClass extends JavaPlugin implements Listener {
 			//comprobar que el tamaño de la lista sea mayor de 3. seleccionar un j8ugador aleatorio y marcarlo como fantasma
 			//coger el resto de los jugadores y meterlos en la lista de los cazafantasmas.
 			// Math.random()*6 
+			
 			int random = (int) (Math.random()*jugadores.size());
 			Player fantasma = jugadores.remove(random);
 			if(!fantasmas.contains(random)){
 				fantasmas.add(fantasma);
 				kitFantasma(fantasma);
-				getServer().broadcastMessage(ChatColor.DARK_RED + "El jugador " + ChatColor.GOLD + fantasma + ChatColor.DARK_RED + " ha muerto por vuestra culpa, y se quiere vengar de vosotros, tener cuidado.");
+				getServer().broadcastMessage(ChatColor.DARK_RED + "El jugador " + ChatColor.GOLD + fantasma.getName() + ChatColor.DARK_RED + " ha muerto por vuestra culpa, y se quiere vengar de vosotros, tener cuidado.");
 
 			}	
 			cazafantasmas.addAll(jugadores);
@@ -58,10 +67,12 @@ public class PluginClass extends JavaPlugin implements Listener {
 				p.sendMessage(ChatColor.GREEN + "Tu objetivo como cazafantasma es sobrevivir a los ataques del fantasma, te hemod prporcionado una linterna que te ayudara a quitarte la ceguera unos 4 segundos, con ella podrás pegar al fantasma.");
 				p.sendMessage(ChatColor.AQUA + "Para utilizar la linterna comoiluminación utiliza:" + ChatColor.GOLD + " Click Derecho" + ChatColor.AQUA + " para utilizarla como arma:" + ChatColor.GOLD + "Click Izquierdo");
 				kitCazafantasma(p);
+				p.setGameMode(GameMode.SURVIVAL);
 			}
 			for (Player p : fantasmas) {
 				p.sendMessage(ChatColor.GREEN + "Eres un fantasma y te tienes que vengar de tus compañeros que no te salvaron, tienes un arma para matarles y tienes una ventaja, no tienes ceguera como ellos que la tienen, su arma es una linterna asi que ten cuidado.");
 				kitFantasma(fantasma);
+				p.setGameMode(GameMode.SURVIVAL);
 			}
 			//cuando mate un fantasma a un gb que lo nmande a un fantasma.
 			BukkitRunnable r = new BukkitRunnable() {
@@ -70,7 +81,9 @@ public class PluginClass extends JavaPlugin implements Listener {
 				public void run() {
 					
 					for (Player p : cazafantasmas){
-						p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS,200,10000));
+						if(!listaNoCiegos.contains(p)){
+							p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS,50000,10000));
+						}
 					}
 				}
 			};
@@ -101,31 +114,33 @@ public class PluginClass extends JavaPlugin implements Listener {
 		im.addEnchant(Enchantment.KNOCKBACK, 1, true);
 		l.setItemMeta(im);
 		p.setItemInHand(l);
-		
+		tiempos.put(p, 0L);
 
 	}
 
-	public void kitFantasma(Player p){
+	public void kitFantasma(Player player){
 		
 		//kit fantasma
 		//quitamos las cosas del jugador
-		p.getInventory().clear();
+		player.getInventory().clear();
 		
 		//añadimos cosas del kit fantasma
 		ItemStack pechera = new ItemStack (Material.CHAINMAIL_CHESTPLATE);
-		p.getInventory().setChestplate(pechera);
+		player.getInventory().setChestplate(pechera);
 		
 		ItemStack patucos = new ItemStack(Material.LEATHER_BOOTS, 1);
 		LeatherArmorMeta lam = (LeatherArmorMeta)patucos.getItemMeta();
 		lam.setColor(Color.fromRGB(255, 255, 255));
 		patucos.setItemMeta(lam);
-		p.getInventory().setBoots(patucos);
+		player.getInventory().setBoots(patucos);
 	
 		
-		ItemStack huesito = new ItemStack(Material.BONE);
-		ItemMeta huesitoMeta = huesito.getItemMeta();
-		huesitoMeta.addEnchant(Enchantment.DAMAGE_ALL, 3,true);
-        p.setItemInHand(huesito);
+		ItemStack hueso = new ItemStack(Material.BONE);
+		ItemMeta huesoMeta = hueso.getItemMeta();
+		huesoMeta.addEnchant(Enchantment.DAMAGE_ALL, 3,true);
+		hueso.setItemMeta(huesoMeta);
+        player.setItemInHand(hueso);
+        
 	}
 	
 	@EventHandler
@@ -155,30 +170,49 @@ public class PluginClass extends JavaPlugin implements Listener {
 
 	}
 
-	@EventHandler
+	/*@EventHandler
 	public void luz (PlayerInteractEvent e){
-		Player p = e.getPlayer();
+		 final Player p = e.getPlayer();
 		if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK){
+			//listaNoCiegos.add(p);
+			p.sendMessage("right click");
 			if(p.getItemInHand()!= null && p.getItemInHand().getItemMeta() != null && p.getItemInHand().getItemMeta().getDisplayName() != null && 
 					p.getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.AQUA + "linterna")){
+				p.sendMessage("identificación iteminhand");
 				long tiempoActual = System.currentTimeMillis();
+				long tiempoUltimoGolpe = 0;
+				if(tiempos.containsKey(p)){
+					tiempoUltimoGolpe = tiempos.get(p);
+				}
 				long tiempoTranscurrido = tiempoActual - tiempoUltimoGolpe;
 				if(tiempoTranscurrido > 4000){
-
-					tiempoUltimoGolpe = System.currentTimeMillis();
+					p.sendMessage("temporizador 1");
+					tiempos.put(p, System.currentTimeMillis());
+					if(!listaNoCiegos.contains(p))
+						listaNoCiegos.add(p);
 					p.removePotionEffect(PotionEffectType.BLINDNESS);
-					long tiempoA = System.currentTimeMillis();
+					BukkitRunnable br = new BukkitRunnable() {
+						
+						@Override
+						public void run() {
+							listaNoCiegos.remove(p);*/
+						/*}
+					};
+					br.runTaskLater(this, 80);*/
+					/*long tiempoA = System.currentTimeMillis();
 					long tiempoT = tiempoActual - tiempoUltimoGolpe;
 					if(tiempoT > 4000){
-
+						p.sendMessage("temporizador 2");
 						tiempoCooldown = System.currentTimeMillis();
 						p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS,20,10000));
 
-					}
-				}
+					}*/
+				/*}
 			}
 		}
 	}
+*/
+
 	
 	@EventHandler
 	public void onBlockPlaceEvent (BlockPlaceEvent e){
@@ -193,7 +227,15 @@ public class PluginClass extends JavaPlugin implements Listener {
      
 
 	
-	
+	@EventHandler
+	public void onBlockBreakEvent (BlockBreakEvent e){
+		
+		if(e.getPlayer().getGameMode() == GameMode.SURVIVAL){
+			e.setCancelled(true);
+		}
+		
+		
+	}
 
 
 
@@ -204,6 +246,20 @@ public class PluginClass extends JavaPlugin implements Listener {
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 
+	
+	
+	@EventHandler
+	public void playerDeaths(PlayerDeathEvent event)
+	{
+		event.getDrops().clear();
+	    /*for(ItemStack drop : (List<ItemStack>).clone())
+	    {
+	        if(drop.getType() == Material.PORTAL)
+	        {
+	            event.getDrops().remove(event.getDrops().indexOf(drop));
+	        }
+	    }*/
+	}
 
 
 }
